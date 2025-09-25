c96acf1626d6ccf19b1ffd4328e8be55ad2c00ba
superkingsely <superkingsely@gmail.com>
2025-09-25 22:08:51 +0100

    data base set next is to run it and edit the serilog and profamcs T-rolemanager

--- SafeHabour.Data/Migrations/20250925210412_InitialCreate.Designer.cs
+++ SafeHabour.Data/Migrations/20250925210412_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SafeHabour.Data.Data;
 
@@ -11,9 +12,11 @@ using SafeHabour.Data.Data;
 namespace SafeHabour.Data.Migrations
 {
     [DbContext(typeof(ApiDbContext))]
-    partial class ApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250925210412_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
