916ae3908bf544caebfd547c2d5de0e88d30d5aa
superkingsely <superkingsely@gmail.com>
2025-09-29 20:15:21 +0100

    upddate database

---
+++ SafeHabour.Data/Migrations/20250929144723_SyncFeatureWithMain.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SafeHabour.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncFeatureWithMain : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
